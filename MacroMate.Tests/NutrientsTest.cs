f793c05472d68ac45b07e7744bd570d11b9efff4
Alex <alexandre.d.paquette+github@gmail.com>
2025-07-28 19:22:57 -0300

    Removed unnecessary  unit tests and added tests for portion property, some fail

--- MacroMate.Tests/NutrientsTest.cs
+++ MacroMate.Tests/NutrientsTest.cs
@@ -7,20 +7,12 @@ public class NutrientsTest
     [TestCase(100, 0, 100, 0, 0)]
     [TestCase(100, 0, 0, 100, 0)]
     [TestCase(100, 0, 0, 0, 100)]
-    [TestCase(100, 100, 100, 0, 0)]
-    [TestCase(100, 100, 0, 100, 0)]
-    [TestCase(100, 100, 0, 0, 100)]
-    [TestCase(100, 0, 100, 0, 100)]
-    [TestCase(100, 0, 0, 100, 100)]
-    [TestCase(100, 0, 100, 100, 100)]
-    [TestCase(100, 100, 0, 100, 100)]
-    [TestCase(100, 100, 100, 0, 100)]
-    [TestCase(100, 100, 100, 100, 0)]
     [TestCase(100, 100, 100, 100, 100)]
     public void NutrientsConstructor_valid(float portion, int cals, float proteins, float carbs, float fats)
     {
         Nutrients test = new(portion, cals, proteins, carbs, fats);
         Assert.That(test.GetType(), Is.EqualTo(typeof(Nutrients)));
+        Assert.That(test.Portion, Is.EqualTo(portion));
         Assert.That(test.Calories, Is.EqualTo(cals));
         Assert.That(test.Proteins, Is.EqualTo(proteins));
         Assert.That(test.Carbohydrates, Is.EqualTo(carbs));
@@ -31,16 +23,9 @@ public class NutrientsTest
     [TestCase(100, 0, -10, 0, 0)]
     [TestCase(100, 0, 0, -10, 0)]
     [TestCase(100, 0, 0, 0, -10)]
-    [TestCase(100, -10, -10, 0, 0)]
-    [TestCase(100, -10, 0, -10, 0)]
-    [TestCase(100, -10, 0, 0, -10)]
-    [TestCase(100, 0, -10, 0, -10)]
-    [TestCase(100, 0, 0, -10, -10)]
-    [TestCase(100, 0, -10, -10, -10)]
-    [TestCase(100, -10, 0, -10, -10)]
-    [TestCase(100, -10, -10, 0, -10)]
-    [TestCase(100, 0, -10, -10, 0)]
     [TestCase(100, -10, -10, -10, -10)]
+    [TestCase(0, 0, 0, 0, 0)]
+    [TestCase(-10, 0, 0, 0, 0)]
     public void NutrientsConstructor_invalid(float portion, int cals, float proteins, float carbs, float fats)
     {
         Assert.Throws<InvalidNutrientValueException>(() => new Nutrients(portion, cals, proteins, carbs, fats));
