eef387684bd951461dc7ae300540dbb3bd153b24
Alex <alexandre.d.paquette+github@gmail.com>
2025-07-28 13:12:36 -0300

    Code: Implement logic to check for negative carb values

--- MacroMate/FoodMacros.cs
+++ MacroMate/FoodMacros.cs
@@ -1,13 +1,30 @@
 namespace MacroMate;
 
-public class FoodMacros(float carbs, float proteins, float fats)
+public class FoodMacros
 {
-    public float Carbs { get; } = carbs;
-    public float Proteins { get; } = proteins;
-    public float Fats { get; } = fats;
+    public float Carbs { get; }
+    public float Proteins { get; }
+    public float Fats { get; }
+
+    public FoodMacros(float carbs, float proteins, float fats)
+    {
+        if (carbs < 0) throw new InvalidMacroValueException("Macros cannot be negative");
+
+        
+        Carbs = carbs;
+        Proteins = proteins;
+        Fats = fats;
+    }
 
     public int Calories()
     {
         return 0;
     }
+}
+
+public class InvalidMacroValueException : Exception
+{
+    public InvalidMacroValueException() { }
+    public InvalidMacroValueException(string msg) : base(msg) { }
+    public InvalidMacroValueException(string msg, Exception inner) : base(msg, inner) { }
 }
