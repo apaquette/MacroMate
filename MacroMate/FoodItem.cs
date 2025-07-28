2811727bad08b864bba224232fc362c1a5c01a7a
Alex <alexandre.d.paquette+github@gmail.com>
2025-07-28 12:54:43 -0300

    Add food macros instance to FoodItem

--- MacroMate/FoodItem.cs
+++ MacroMate/FoodItem.cs
@@ -1,11 +1,9 @@
 ﻿namespace MacroMate;
 
-public class FoodItem(string name, float carbs, float proteins, float fats, DateTime date) : ILogItem
+public class FoodItem(string name, DateTime date) : ILogItem
 {
     public string Name { get; } = name;
-    public float Carbs { get; } = carbs;
-    public float Proteins { get; } = proteins;
-    public float Fats { get; } = fats;
+    public FoodMacros Macros { get; }
     public DateTime Date { get; } = date;
 
     public void Display()
