c37893771e5d843f3e55c98d2bd1193aea9a6413
Alex <alexandre.d.paquette+github@gmail.com>
2025-07-28 12:55:35 -0300

    update unit tests to test FoodMacros class

---
+++ MacroMate.Tests/FoodMacrosTest.cs
@@ -0,0 +1,20 @@
+namespace MacroMate.Tests;
+public class FoodMacrosTests
+{
+    [TestCase(0, 0, 0)]
+    [TestCase(100, 0, 0)]
+    [TestCase(0, 100, 0)]
+    [TestCase(0, 0, 100)]
+    [TestCase(100, 100, 0)]
+    [TestCase(100, 0, 100)]
+    [TestCase(0, 100, 100)]
+    [TestCase(100, 100, 100)]
+    public void FoodMacrosConstructor_valid(float carbs, float proteins, float fats)
+    {
+        FoodMacros test = new(carbs, proteins, fats);
+        Assert.That(test.GetType(), Is.EqualTo(typeof(FoodMacros)));
+        Assert.That(test.Carbs, Is.EqualTo(carbs));
+        Assert.That(test.Fats, Is.EqualTo(fats));
+        Assert.That(test.Proteins, Is.EqualTo(proteins));
+    }
+}
