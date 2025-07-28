998300e1fe4186daee6f9d11d5e3e3651265c751
Alex <alexandre.d.paquette+github@gmail.com>
2025-07-28 12:31:48 -0300

    Implement ILogItem interface in FoodItem

--- MacroMate/FoodItem.cs
+++ MacroMate/FoodItem.cs
@@ -1,10 +1,15 @@
 ﻿namespace MacroMate;
 
-public class FoodItem(string name, float carbs, float proteins, float fats, DateTime date)
+public class FoodItem(string name, float carbs, float proteins, float fats, DateTime date) : ILogItem
 {
     public string Name { get; } = name;
     public float Carbs { get; } = carbs;
     public float Proteins { get; } = proteins;
     public float Fats { get; } = fats;
     public DateTime Date { get; } = date;
+
+    public void Display()
+    {
+        throw new NotImplementedException();
+    }
 }
