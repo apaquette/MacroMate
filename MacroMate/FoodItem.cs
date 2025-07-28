f951535d3eb929edc2f699ea60a89c369e871798
Alex <alexandre.d.paquette+github@gmail.com>
2025-07-27 22:47:32 -0300

    add private fields

--- MacroMate/FoodItem.cs
+++ MacroMate/FoodItem.cs
@@ -2,8 +2,13 @@
 
 public class FoodItem
 {
+    private string name;
+    private float carbs;
+    private float proteins;
+    private float fats;
+    private DateTime date;
     public FoodItem(string name, float carbs, float proteins, float fats, DateTime date)
     {
-
+        
     }
 }
