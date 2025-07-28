9dc35b1dc8cd72eb0d783d897d64929f38ff57e0
Alex <alexandre.d.paquette+github@gmail.com>
2025-07-27 22:55:13 -0300

    Test: create first unit test for valid input of all zero macros, fails

--- MacroMate.Tests/FoodItemTest.cs
+++ MacroMate.Tests/FoodItemTest.cs
@@ -1,10 +1,15 @@
 namespace MacroMate.Tests;
-
 public class FoodItemTests
 {
-    [Test]
-    public void Test1()
+    [TestCase("Dubious", 0, 0, 0, "2025, 1, 1")]
+    public void FoodItemConstructor_valid(string name, float carbs, float proteins, float fats, DateTime date)
     {
-        
-    } 
+        FoodItem test = new(name, carbs, proteins, fats, date);
+        Assert.That(test.GetType(), Is.EqualTo(typeof(FoodItem)));
+        Assert.That(test.Name, Is.EqualTo(name));
+        Assert.That(test.Carbs, Is.EqualTo(carbs));
+        Assert.That(test.Fats, Is.EqualTo(fats));
+        Assert.That(test.Proteins, Is.EqualTo(proteins));
+        Assert.That(test.Date, Is.EqualTo(date));
+    }
 }
